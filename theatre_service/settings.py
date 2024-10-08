ab312a2da3684d8e3aea120919c0e9cdf2e91b63
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-08 20:17:15 +0300

    add user and theatre to INSTALLED_APPS, make migrations and migrate

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "theatre",
+    "user",
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
