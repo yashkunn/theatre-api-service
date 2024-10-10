69901f2922db8aaec47fe3765df3fa8563ca9721
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 15:21:36 +0300

    add debug_toolbar

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -27,6 +27,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "localhost",
+]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+}
 
 # Application definition
 
@@ -40,6 +53,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "drf_spectacular",
+    "debug_toolbar",
 
     "theatre",
     "user",
@@ -49,6 +63,7 @@ AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+
+from theatre_service import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
