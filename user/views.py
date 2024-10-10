f2cf79ca326116878cefc1bd277bedc37a52eb29
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 16:06:28 +0300

    add permission for CreateUser

--- user/views.py
+++ user/views.py
@@ -2,11 +2,13 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from theatre.permissions import CreateUserPermission
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [CreateUserPermission]
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
