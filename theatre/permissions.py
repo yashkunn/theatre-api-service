93917a49d7e4ffafd406f0dfd0940bbec636297e
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 15:31:58 +0300

    change permissions.py

--- theatre/permissions.py
+++ theatre/permissions.py
@@ -3,14 +3,10 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.user and request.user.is_staff:
+        if request.method in SAFE_METHODS:
             return True
 
-        if (
-            request.method in SAFE_METHODS
-            and request.user
-            and request.user.is_authenticated
-        ):
+        if request.user and request.user.is_staff:
             return True
 
         return False
