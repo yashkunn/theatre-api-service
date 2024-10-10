85b1671b00508001da966009bf35488be732da8b
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 15:12:40 +0300

    rename and change permissions

--- theatre/permissions.py
+++ theatre/permissions.py
@@ -1,13 +1,16 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
-            )
-            or (request.user and request.user.is_staff)
-        )
+        if request.user and request.user.is_staff:
+            return True
+
+        if (
+            request.method in SAFE_METHODS
+            and request.user
+            and request.user.is_authenticated
+        ):
+            return True
+
+        return False
