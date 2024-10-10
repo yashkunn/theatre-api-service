5d8b0a89d46b34b269330a5c43aedf17d5c05b82
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 15:12:58 +0300

    add permissions to settings.py

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
+    "drf_spectacular",
 
     "theatre",
     "user",
@@ -127,3 +129,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": (
+        "theatre.permissions.IsAdminOrReadOnly",
+    ),
+}
