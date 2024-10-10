9b6093aa66ed9c5be7315de24f54f093aec214d9
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 15:42:43 +0300

    add configurations to User(AbstractUser)

--- user/models.py
+++ user/models.py
@@ -1,5 +1,17 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractUser,
+)
+from django.db import models
+from django.utils.translation import gettext as _
+
+from user.managers import UserManager
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
