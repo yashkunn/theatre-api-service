8428fef62cbe5827bd115e9d8868a6918af52763
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 15:54:35 +0300

    add SIMPLE_JWT

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -179,7 +179,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
