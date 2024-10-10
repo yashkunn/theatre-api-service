abc5c3bf3a766173734498de16f95f2586006478
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-10 15:51:29 +0300

    add THROTTLE

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -154,6 +154,11 @@ REST_FRAMEWORK = {
         "theatre.permissions.IsAdminOrReadOnly",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 SPECTACULAR_SETTINGS = {
